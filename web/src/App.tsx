d6f8812f9bf994511b0d332f22ea5a3ad799834b
juancjara <juancjara@pucp.pe>
2019-08-24 15:04:52 -0500

    first commit frontend

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Steam achievements</title>
+      </Helmet>
+      <h1>App</h1>
+    </>
+  );
+};
+
+export default App;
